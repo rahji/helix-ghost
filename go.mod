b8e5b8984af4547856db3e1345c0be57dc8e8738
Rob Duarte <me@robduarte.com>
2025-05-24 23:42:37 -0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module helix-ghost
+
+go 1.23.3
+
+require (
+	github.com/alecthomas/kong v1.11.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+
+	"github.com/alecthomas/kong"
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+)
+
+type CLIFlags struct {
+	Port    int  `kong:"default=4001,name='http-port',help='HTTP port'"`
+	Verbose bool `kong:"name='verbose',help='Show extra output in the terminal'"`
+}
+
+type GhostText struct {
+	Filename string
+	Title    string `json:"title"`
+	Text     string `json:"text"`
+}
+
+func main() {
+	var cli CLIFlags
+	kong.Parse(&cli)
+
+	// Handle the initial GET request on port specified by Port flag
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		// Find an available port for WebSocket
+		wsListener, err := net.Listen("tcp", "127.0.0.1:0") // Random available port
+		if err != nil {
+			http.Error(w, "Failed to find available port", http.StatusInternalServerError)
+			return
+		}
+
+		wsPort := wsListener.Addr().(*net.TCPAddr).Port
+		log.Printf("WebSocket will listen on port %d", wsPort)
+
+		// Start the WebSocket listener in the background
+		go handleWebSocketConnections(wsListener)
+
+		// Respond to client with JSON payload described in GhostText PROTOCOL
+		resp := map[string]interface{}{
+			"WebSocketPort":   wsPort,
+			"ProtocolVersion": 1,
+		}
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(resp)
+	})
+
+	port := fmt.Sprintf(":%d", cli.Port)
+	log.Println("HTTP server listening on port ", port)
+	if err := http.ListenAndServe(port, nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleWebSocketConnections(ln net.Listener) {
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Println("Failed to accept connection:", err)
+			continue
+		}
+
+		// Perform WebSocket upgrade
+		_, err = ws.Upgrade(conn)
+		if err != nil {
+			log.Println("WebSocket upgrade failed:", err)
+			conn.Close()
+			continue
+		}
+
+		log.Println("WebSocket connection established")
+
+		go func(c net.Conn) {
+			defer c.Close()
+			for {
+				msg, op, err := wsutil.ReadClientData(c)
+				if err != nil {
+					log.Println("Read error:", err)
+					return
+				}
+				var incoming GhostText
+				json.Unmarshal(msg, &incoming)
+				log.Printf("Title: %s", incoming.Title)
+
+				err = wsutil.WriteServerMessage(c, op, []byte("Echo: "+string(msg)))
+				if err != nil {
+					log.Println("Write error:", err)
+					return
+				}
+			}
+		}(conn)
+	}
+}
